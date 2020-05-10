eb1dabec6d02bf18a9fd9aac2bd24f1a56c13c08
Krittanan Chalong <42577829+ice5564ice@users.noreply.github.com>
2020-05-11 02:11:33 +0700

    Update AttendeesForm.php

--- form/AttendeesForm.php
+++ form/AttendeesForm.php
@@ -5,5 +5,21 @@
 	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 	header('Content-type: application/json');
 
+	$servername = "us-cdbr-iron-east-01.cleardb.net";
+	$dbusername = "b30ea52d3aa952";
+	$dbpassword = "918a5c13";
+	$dbname = "heroku_1c39a65d43a5546";
+	$conn = mysqli_connect($servername,$dbusername,$dbpassword,$dbname);
+	
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$personalID = null;
+	$conferenceID - null;
+	$startime = null;
+	$endtime = null;
+
+	$sql = "INSERT INTO "
 
 ?>
