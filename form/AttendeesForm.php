68709ff82e84922a828b7afb7667dcb1cd6ec0ba
Krittanan Chalong <42577829+ice5564ice@users.noreply.github.com>
2020-05-11 00:55:42 +0700

    Update AttendeesForm.php

--- form/AttendeesForm.php
+++ form/AttendeesForm.php
@@ -1,5 +1,9 @@
 <?php
 
+  header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+	header('Content-type: application/json');
 
 
 ?>
