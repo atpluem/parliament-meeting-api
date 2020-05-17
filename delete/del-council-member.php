f650347570c67601b3d4dd5da74b456395ab6443
atpluem <45143217+atpluem@users.noreply.github.com>
2020-05-17 21:29:01 +0700

    Update del-council-member.php

--- delete/del-council-member.php
+++ delete/del-council-member.php
@@ -7,7 +7,6 @@
   WHERE PersonalID = $personalid;";
   
   if($mysqli->query($sql) === TRUE) {
-    print_r("Record deleted successfully");
     echo "Record deleted successfully";
   } else {
     echo "Error deleting record: " . $mysqli->error;
