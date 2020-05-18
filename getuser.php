53d7a0bcd794bfb73a377f2479b57b8ec52b31cc
atpluem <45143217+atpluem@users.noreply.github.com>
2020-05-18 23:14:57 +0700

    Update getuser.php

--- getuser.php
+++ getuser.php
@@ -4,11 +4,24 @@
 $postdata = file_get_contents("php://input");
 $personalid=$_GET["username"];
  
-$sql="SELECT PersonalID,PartyName,p.PartyPosName,SubMinistryName,c.CouncilposName,mn.MinistryPosName,
+$sql="SELECT PersonalID,PartyName,
+       CASE  WHEN m.PartyPosID IS NOT NULL
+            THEN p.PartyPosName 
+            ELSE NULL END AS PartyPosName
+        ,SubMinistryName,
+        CASE  WHEN m.CouncilPosID IS NOT NULL
+            THEN c.CouncilposName
+            ELSE NULL END AS CouncilposName
+        ,CASE  WHEN m.MinistryPosID  IS NOT NULL
+            THEN mn.MinistryPosName
+            ELSE NULL END AS MinistryPosName,
         Surname,Lastname,DOB,EducationDegree,m.Password,MemberPicture
 FROM councilmember m,PartyPos p,councilpos c,ministrypos mn
-WHERE m.PartyPosID = p.PartyPosID AND m.CouncilPosID = c.CouncilPosID
-        AND m.MinistryPosID = mn.MinistryPosID AND m.PersonalID = $personalid";
+WHERE (m.PartyPosID = p.PartyPosID OR m.PartyPosID IS NULL)
+     AND (m.CouncilPosID = c.CouncilPosID OR m.CouncilPosID  IS NULL)
+     AND (m.MinistryPosID = mn.MinistryPosID  OR m.MinistryPosID IS NULL)
+     AND m.PersonalID = $personalid
+LIMIT 1;";
  
 if($result = mysqli_query($mysqli,$sql))
 {
