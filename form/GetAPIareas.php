1ff5c401748409df46b41ea2cff92f7833ddc1b5
atpluem <45143217+atpluem@users.noreply.github.com>
2020-05-18 12:21:42 +0700

    Update GetAPIareas.php

--- form/GetAPIareas.php
+++ form/GetAPIareas.php
@@ -4,10 +4,10 @@ header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Content-type: application/json');
 
-$servername = "us-cdbr-iron-east-01.cleardb.net";
-$dbusername = "b30ea52d3aa952";
-$dbpassword = "918a5c13";
-$dbname = "heroku_1c39a65d43a5546";
+$servername = "us-cdbr-east-06.cleardb.net";
+$dbusername = "b6f44590f309b7";
+$dbpassword = "df25ea47";
+$dbname = "heroku_081a4c7d902d08f";
 $conn = mysqli_connect($servername,$dbusername,$dbpassword,$dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
