ec3bf13bb51ddfce8f4274eb94418dcc26b34679
atpluem <45143217+atpluem@users.noreply.github.com>
2020-05-18 23:01:16 +0700

    Create editpass.php

---
+++ editpass.php
@@ -0,0 +1,42 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+	header('Content-type: application/json');
+	
+	$servername = "us-cdbr-east-06.cleardb.net";
+	$dbusername = "b6f44590f309b7";
+	$dbpassword = "df25ea47";
+	$dbname = "heroku_081a4c7d902d08f";
+	$conn = mysqli_connect($servername,$dbusername,$dbpassword,$dbname);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+  
+  $Password = null;
+  $NewPassword = null;
+
+	$data = file_get_contents("php://input");
+	$array = json_decode($data,true);
+	print_r($array);
+  
+	foreach($array[jsonform] as $key => $output) {
+		if($key === 'password') {
+			$Password = $output;
+		}
+    else if($key === 'newpassword') {
+      $NewPassword = $output;
+    }
+	} 
+
+$sql = "UPDATE councilmember
+SET Password = $NewPassword WHERE Password = $Password";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Change password successfully";
+} else {
+     echo "Error: " . $sql . "<br>" . $conn->error;
+}
+ $conn->close();
+
+?>
