b9eb16b1ef0d765eb4c2bea0023e4d782c2308cf
atpluem <45143217+atpluem@users.noreply.github.com>
2020-05-18 10:27:37 +0700

    Update del-council-member.php

--- delete/del-council-member.php
+++ delete/del-council-member.php
@@ -1,11 +1,9 @@
 <?php 
-  // Test serv
   include_once("../config.php");
   $postdata = file_get_contents("php://input");
   $personalid=$_GET["username"];
   
-  $sql = "DELETE FROM councilmember
-  WHERE PersonalID = $personalid;";
+  $sql = "DELETE FROM councilmember WHERE PersonalID = $personalid;";
   
   if($mysqli->query($sql) === true) {
     echo "Record deleted successfully";
