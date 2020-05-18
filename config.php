6657c22fa1fa64c9c2001a991d0702a32d2f4533
atpluem <45143217+atpluem@users.noreply.github.com>
2020-05-18 12:18:02 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 	header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
  
-	$db_username = 'b30ea52d3aa952';
-	$db_password = '918a5c13';
-	$db_name = 'heroku_1c39a65d43a5546';
-	$db_host = 'us-cdbr-iron-east-01.cleardb.net';				
+	$db_username = 'b6f44590f309b7';
+	$db_password = 'df25ea47';
+	$db_name = 'heroku_081a4c7d902d08f';
+	$db_host = 'us-cdbr-east-06.cleardb.net';				
 	$mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
  
 	if ($mysqli->connect_error) {
