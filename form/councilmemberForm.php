5bada17f9a58fbd68ecf67c838e1374ac2fd5d5d
atpluem <45143217+atpluem@users.noreply.github.com>
2020-05-18 21:59:17 +0700

    Add files via upload

--- form/councilmemberForm.php
+++ form/councilmemberForm.php
@@ -5,10 +5,10 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 header('Content-type: application/json');
 
 $servername = "us-cdbr-east-06.cleardb.net";
-	$dbusername = "b6f44590f309b7";
-	$dbpassword = "df25ea47";
-	$dbname = "heroku_081a4c7d902d08f";
-	$conn = mysqli_connect($servername,$dbusername,$dbpassword,$dbname);
+$dbusername = "b6f44590f309b7";
+$dbpassword = "df25ea47";
+$dbname = "heroku_081a4c7d902d08f";
+$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
@@ -25,9 +25,19 @@ $image = null;
 $password = null;
 $PartyPosID = null;
 
+$editclick = null;
+$addclick = null;
+$deleteclick = null;
+
 $data = file_get_contents("php://input");
 $array = json_decode($data, true);
-//print_r($array);
+print_r($array);
+$addclick = $array["addclick"];
+$editclick = $array["editclick"];
+$deleteclick = $array["deleteclick"];
+
+//print($addclick . $editclick . $deleteclick);
+
 foreach ($array["jsonform"] as $key => $output) {
 	if ($key === 'id') {
 		$id = $output;
@@ -57,7 +67,7 @@ foreach ($array["jsonform"] as $key => $output) {
 		//echo $partyname."\n";
 	} elseif ($key === 'subministryname') {
 		$subministryname = $output;
-		echo $subministryname."\n";
+		echo $subministryname . "\n";
 	} elseif ($key === 'ministrypos') {
 		foreach ($output as $key => $result) {
 			if ($key == 'MinistryPosID') {
@@ -78,21 +88,77 @@ foreach ($array["jsonform"] as $key => $output) {
 		}
 	}
 }
+
+print_r($partyname,$PartyPosID,$subministryname,$ministrypos);
+if($partyname == null){
+	$partyname = "NULL";
+}
+else{
+	$partyname = "'".$partyname."'";
+}
+
+if($PartyPosID == null){
+	$PartyPosID = "NULL";
+}
+
+if($subministryname == null){
+	$subministryname = "NULL";
+}
+else{
+	$subministryname = "'".$subministryname."'";
+}
+
+if($ministrypos == null){
+	$ministrypos = "NULL";
+}
+
+
 $image = $array["image"];
 $image = explode(',', $image);
 $image = base64_encode($image[1]);
 $image = base64_decode($image);
 //echo $image."\n";
 
-$sql = "INSERT INTO councilmember (personalID,PartyName,PartyPosID,SubministryName,CouncilPosID,
+if ($addclick == true) {
+	$sql = "INSERT INTO councilmember (personalID,PartyName,PartyPosID,SubministryName,CouncilPosID,
 MinistryPosID,Surname,Lastname,DOB,EducationDegree,Password,MemberPicture)
-VALUES ('$id','$partyname',$PartyPosID,'$subministryname',$councilpos,$ministrypos,'$firstname','$lastname','$dob','$education','$password','$image')";
+VALUES ('$id',$partyname,$PartyPosID,$subministryname,$councilpos,$ministrypos,'$firstname','$lastname','$dob','$education','$password','$image')";
 
-if ($conn->query($sql) === TRUE) {
-	echo "New record created successfully";
-} else {
-	echo "Error: " . $sql . "<br>" . $conn->error;
+	if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+} elseif ($editclick == true) {
+	$sql = "SELECT personalID FROM councilmember ";
+	$result = mysqli_query($conn, $sql) or die("Bad query: $sql");
+	while ($row = mysqli_fetch_assoc($result)) {
+		if ($id == "{$row['personalID']}") {
+			$sql = "UPDATE councilmember
+			SET PartyName = $partyname, PartyPosID = $PartyPosID,SubministryName = $subministryname,CouncilPosID = $councilpos,
+MinistryPosID = $ministrypos,Surname='$firstname',Lastname = '$lastname',DOB = '$dob',EducationDegree = '$education', Password = '$password', MemberPicture = '$image'
+			WHERE personalID = '$id'";
+			if ($conn->query($sql) === TRUE) {
+				echo "New record created successfully";
+			} else {
+				echo "Error: " . $sql . "<br>" . $conn->error;
+			}
+		}
+	}
+}elseif($deleteclick == true){
+	$sql = "SELECT personalID FROM councilmember ";
+	$result = mysqli_query($conn, $sql) or die("Bad query: $sql");
+	while ($row = mysqli_fetch_assoc($result)) {
+		if ($id == "{$row['personalID']}") {
+			$sql = "DELETE FROM councilmember WHERE personalID = '$id'";
+			if ($conn->query($sql) === TRUE) {
+				echo "New record created successfully";
+			} else {
+				echo "Error: " . $sql . "<br>" . $conn->error;
+			}
+		}
+	}
 }
-$conn->close();
 
-?>
+
+$conn->close();
