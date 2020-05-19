406111d885117204250da48871bd650f9fb782a0
atpluem <45143217+atpluem@users.noreply.github.com>
2020-05-19 11:20:02 +0700

    Update editpass.php

--- editpass.php
+++ editpass.php
@@ -42,13 +42,8 @@ SET Password = $NewPassword WHERE Password = $Password AND PersonalID = $Persona
 if ($result = mysqli_query($conn,$sql)) {
 	$row = mysqli_fetch_assoc($result);
 	print_r($row);
-	if($row != null){
-		print("success")
 		$conn->query($sqlupdate);
-	}
-	else {
-		print("fail");
-	}
+
 } else {
      echo "Error: " . $sql . "<br>" . $conn->error;
 }
